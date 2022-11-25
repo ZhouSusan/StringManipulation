71c4d8f636f702b9e9c4110be04c9baed26c59cb
ZhouSusan <szhou089@gmail.com>
2022-11-24 22:18:25 -0800

    added trimAndConcat() method to StringManipulator class (StringManipulator.java)

--- src/main/java/StringManipulator/StringManipulator.java
+++ src/main/java/StringManipulator/StringManipulator.java
@@ -1,5 +1,8 @@
 package StringManipulator;
 
 public class StringManipulator {
-    
+    public String trimAndConcat(String str1, String str2) {
+        String concatStr = str1.trim() + str2.trim();
+        return concatStr;
+    }
 }
