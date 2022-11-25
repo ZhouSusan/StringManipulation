5737e4734c5314f41cdf10b05048211b62398688
ZhouSusan <szhou089@gmail.com>
2022-11-24 22:51:31 -0800

    added to two more testcases in StringManipulatorTest.java

--- src/test/java/StringManipulator/StringManipulatorTest.java
+++ src/test/java/StringManipulator/StringManipulatorTest.java
@@ -7,12 +7,16 @@ class StringManipulatorTest {
     @org.junit.jupiter.api.Test
     void trimAndConcatTest() {
         StringManipulator trimConcatTestCase1 = new StringManipulator();
+        StringManipulator trimConcatTestCase2 = new StringManipulator();
         assertEquals("HelloWorld", trimConcatTestCase1.trimAndConcat("     Hello    ", "      World"   ));
+        assertEquals("HappyThanksgiving!", trimConcatTestCase2.trimAndConcat("     Happy","Thanksgiving!      "));
     }
 
     void getIndexOrNull() {
         StringManipulator getIndexOfChar = new StringManipulator();
         int indexOfChar = getIndexOfChar.getIndexOrNull("Happy Gobble Gobble Turkey Day!", 'G');
+        int indexOfChar2 = getIndexOfChar.getIndexOrNull("Happy Gobble Gobble Turkey Day!", 'z');
         assertEquals(7, indexOfChar);
+        assertEquals(-1, indexOfChar2);
     }
 }
